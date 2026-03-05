df9a0b07ac729b4909ac95cee5d0ec38427bdac8
asocialtr-glitch <asocialtr@gmail.com>
2026-03-05 20:01:23 +0300

    Resolve null pointer exception in user service

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T17:00:39.002Z
-Commit: Add multi-language support
+Timestamp: 2026-03-05T17:01:24.534Z
+Commit: Resolve null pointer exception in user service
