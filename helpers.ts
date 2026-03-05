b3f6fbdab243ed43e8b6f45509b70b37386de39f
asocialtr-glitch <asocialtr@gmail.com>
2026-03-05 20:03:17 +0300

    Resolve null pointer exception in user service

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T17:02:59.969Z
-Commit: Fix memory leak in event handlers
+Timestamp: 2026-03-05T17:03:18.944Z
+Commit: Resolve null pointer exception in user service
