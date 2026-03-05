3159984e40a3d92b0910d3f47fe196e14c1b68cb
asocialtr-glitch <asocialtr@gmail.com>
2026-03-05 20:04:26 +0300

    Fix CSS styling bug on mobile devices

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T17:04:16.523Z
-Commit: Correct typo in configuration file
+Timestamp: 2026-03-05T17:04:27.826Z
+Commit: Fix CSS styling bug on mobile devices
