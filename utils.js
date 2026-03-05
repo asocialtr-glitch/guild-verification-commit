bbc5868c39abc2092d53a626ab9ab538915f48fa
asocialtr-glitch <asocialtr@gmail.com>
2026-03-05 20:01:13 +0300

    Fix race condition in async operations

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T17:01:07.954Z
-Commit: Introduce real-time notifications
+Timestamp: 2026-03-05T17:01:14.128Z
+Commit: Fix race condition in async operations
