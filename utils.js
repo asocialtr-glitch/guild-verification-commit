daba2ecaab2be9fb0c36a2225ac7b709e6923581
asocialtr-glitch <asocialtr@gmail.com>
2026-03-05 20:03:06 +0300

    Patch API endpoint error handling

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T17:03:05.578Z
-Commit: Fix broken links in documentation
+Timestamp: 2026-03-05T17:03:07.465Z
+Commit: Patch API endpoint error handling
